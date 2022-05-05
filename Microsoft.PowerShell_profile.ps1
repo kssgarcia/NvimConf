5825dbca8699d152c7dbc946a5d8a7f17164269b
Kevin <santyagogarcia.777@gmail.com>
2022-05-04 20:41:48 -0500

    first commit

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,34 @@
+# Theme
+Import-Module Terminal-Icons
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-PoshPrompt fish 
+
+#function Get-ScriptDirection { Split-Path $MyInvocation.ScriptName }
+#$PROMPT_CONFIG = Join-Path (Get-ScriptDirection) "santiago.omp.json" 
+#oh-my-posh --init --shell pwsh --config $PROMPT_CONFIG | Invoke-Expression
+
+# PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineOption -PredictionSource History
+
+# Z module
+# Install-Module -Name z -Force
+
+# Fzf
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+# Utilities
+function which ($command) {
+  Get-Command -Name $command -ErrorAction SilentlyContinue |
+    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
